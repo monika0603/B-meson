7e50527d4068a656dd1dbc16d75c4d94676af008
monika0603 <monika.pu@gmail.com>
2015-05-14 09:53:05 -0400

    Solving fatal error

--- stepITagAndProbeTreeProducer_JPsiMuMu.py
+++ stepITagAndProbeTreeProducer_JPsiMuMu.py
@@ -33,7 +33,7 @@ elif "CMSSW_7_4_" in os.environ['CMSSW_VERSION']:
     process.GlobalTag.globaltag = cms.string('MCRUN2_74_V7')
     process.source.fileNames = [
         # J/psi->mumu
-        '/store/mc/Phys14DR/JpsiToMuMu_JPsiPt7WithFSR_13TeV-pythia6-evtgen/AODSIM/AVE30BX50_tsg_PHYS14_ST_V1-v2/00000/0881BD04-A899-E411-9E89-002590A887FE.root',
+        '/store/mc/Phys14DR/JpsiToMuMu_JPsiPt7WithFSR_13TeV-pythia6-evtgen/AODSIM/AVE30BX50_tsg_PHYS14_ST_V1-v2/00000/30F15644-9199-E411-8789-001E67398958.root',
                                 # '/store/mc/Summer12_DR53X/BuToJPsiK_K2MuPtEtaEtaFilter_8TeV-pythia6-evtgen/AODSIM/PU_S10_START53_V7A-v2/0000/008A75FC-E1DE-E111-A9C7-00215E2213EC.root',
                                 #'/store/mc/Summer12_DR53X/BuToJPsiK_K2MuPtEtaEtaFilter_8TeV-pythia6-evtgen/AODSIM/PU_S10_START53_V7A-v2/0000/00A8B265-EBDE-E111-BC43-001A645BE078.root',
     ]
